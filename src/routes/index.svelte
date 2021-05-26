12293bc9de8bfb87602d614a6d5c86a584929183
miunau <miunau@miunau.com>
2021-05-26 07:11:04 +0200

    link to manual instead of faq on home page and update wording

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -43,7 +43,7 @@
   <p>It fits into 24HP eurorack cases, but can also be played as a standalone desktop mini-synth, with line-level stereo input and a headphone output, all powered from a micro-USB connector.</p>
 
   <h4>When is it available?</h4>
-  <p>As it is a hobby project at heart, the DIY kits are made in limited quantities. The first run of 100 sold out in December 2020. There is a second run of 200 available end of May 2021, <a href="https://www.thonk.co.uk/plinky-2021/" target="_blank">with more on the way</a>. You can wishlist it at Thonk.</p>
+  <p>As it is a hobby project at heart, the DIY kits are made in limited quantities. The first run of 100 sold out in December 2020. There was a second run of 200 made end of May 2021, <a href="https://www.thonk.co.uk/plinky-2021/" target="_blank">with more on the way</a>. You can wishlist it at Thonk, who will notify you of new stock.</p>
   <a class="button" target="_blank" href="https://www.thonk.co.uk/shop/plinky/">Wishlist at Thonk</a>
 </TextAndImage>
 
@@ -62,7 +62,7 @@
     <h3>Firmware</h3>
     <p>The latest firmware version is {firmware.stable.version} released on {firmware.stable.date}.</p>
   </LinkBlock>
-  <LinkBlock href="/docs/build-guide" label="Building instructions">
+  <LinkBlock href="/docs/build-guide-blue" label="Building instructions">
     <h3>Building</h3>
     <p>Building your Plinky is part of the fun, and easy!</p>
   </LinkBlock>
@@ -70,9 +70,9 @@
     <h3>How to play</h3>
     <p>Got your Plinky working? Great! Go here to learn how to play it.</p>
   </LinkBlock>
-  <LinkBlock href="/docs/faq" label="FAQ">
-    <h3>FAQ</h3>
-    <p>Find all the little gotchas and tidbits here.</p>
+  <LinkBlock href="/docs/manual" label="Manual">
+    <h3>Manual</h3>
+    <p>Read the Plinky manual.</p>
   </LinkBlock>
 </Blocks>
 
