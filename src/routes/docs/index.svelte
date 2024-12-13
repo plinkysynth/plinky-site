05dea7812f58e57697c54638323e1f760ba8ee34
Alex Evans <715855+mmalex@users.noreply.github.com>
2024-12-13 10:29:52 -0500

    temporary manual docs page

--- src/routes/docs/index.svelte
+++ src/routes/docs/index.svelte
@@ -15,12 +15,37 @@
 </svelte:head>
 
 <div class="page">
-  <h1>Documentation</h1>
-  {#each posts as post, index}
-    <div class="post-item">
-      <h2>
-        <a href='docs/{post.slug}'>{post.title}</a>
-      </h2>
-    </div>
-  {/each}
+    <h1>Documentation - Plinky</h1>
+    <h2>Plinky Manual & Build guides</h2>
+    <div class="post-item"><h3><a href="docs/manual">Plinky Manual</a></h3></div>
+    <div class="post-item"><h3><a href="docs/build-guide-black">Plinky build guide - V3 Black (new)</a></h3></div>
+    <div class="post-item"><h3><a href="docs/build-guide-blue">Plinky build guide - V2 Blue (old)</a></h3></div>
+    <div class="post-item"><h3><a href="docs/build-guide">Plinky build guide - V1 Green (old)</a></h3></div>
+    <div class="post-item"><h3><a href="docs/expander-build-guide">Plinky Expander build guide</a></h3></div>
+    <br/>
+    <h2>Plinky More information</h2>
+    <div class="post-item"><h3><a href="docs/midi">MIDI</a></h3></div>
+    <div class="post-item"><h3><a href="docs/editors">Plinky Editors</a></h3></div>
+    <div class="post-item"><h3><a href="docs/faq">FAQ</a></h3></div>
+    <div class="post-item"><h3><a href="docs/play-guide">Plinky Play guide</a></h3></div>
+    <br/>
+    <h1>Documentation - Buddies</h1>
+    <br/>
+    <h2>Bib</h2>
+    <div class="post-item"><h3><a href="docs/bib-manual">Bib Manual</a></h3></div>
+    <div class="post-item"><h3><a href="docs/build-guide_bib">Bib build guide - V2</a></h3></div>
+    <div class="post-item"><h3><a href="docs/build-guide_bib_v1">Bib build guide - V1</a></h3></div>
+    <br/>
+    <h2>Buzzrito</h2>
+    <div class="post-item"><h3><a href="docs/buzzrito-manual">Buzzrito Manual</a></h3></div>
+    <div class="post-item"><h3><a href="docs/build-guide_buzzy">Buzzrito build guide - V1</a></h3></div>
+    <br/>
+    <h2>Blueberry</h2>
+    <div class="post-item"><h3><a href="docs/blueberry-manual">Blueberry Manual</a></h3></div>
+    <div class="post-item"><h3><a href="docs/build-guide_blueberry">Blueberry build guide - V1</a></h3></div>
+    <br/>
+    <h2>3U combo panel</h2>
+    <div class="post-item"><h3><a href="docs/build-guide_combo">3U Bib-Buzzrito-Blueberry Combo build guide</a></h3></div>
+    
+
 </div>
